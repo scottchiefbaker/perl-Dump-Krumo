87e990a033aa0918c865a5079ed1914baa95de91
Scott Baker <scott@perturb.org>
2025-11-12 15:49:40 -0800

    More accuate scalar references (rare)

--- lib/Dump/Krumo.pm
+++ lib/Dump/Krumo.pm
@@ -110,7 +110,7 @@ sub __dump {
 	} elsif ($type eq 'HASH') {
 		$ret = __dump_hash($x);
 	} elsif ($type eq 'SCALAR') {
-		$ret = color($COLORS->{scalar_ref}, "Scalar reference: '$$x'");
+		$ret = color($COLORS->{scalar_ref}, '\\' . quote_string($$x));
 	} elsif (!$type && is_bool_val($x)) {
 		$ret = __dump_bool($x);
 	} elsif (!$type && is_integer($x)) {
